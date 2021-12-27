f9922d381049ab0c0319a9d6ea51fa8c682ae2d3
Nisa Hayuningrum <nisahayuningrum.nh@gmail.com>
2021-12-27 22:21:03 +0700

    delete notaris

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotarisController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+// --CRUD Notaris
+Route::resource('/dashboard/notaris', NotarisController::class);
